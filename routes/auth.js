c262e2ba2e1b9a732f7cf3fb1bfd487e8d4693e5
Eugenia Musiimemaria <eugeniamusiimemaria@gmail.com>
2021-07-25 17:47:10 +0300

    Updating auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,22 @@
-const Matcher = require()
+const Manager = require("../models/managerModel")
 
-const auth = (req, res) => {
-    let user 
-    if(user === 1){
-        res.redirect("/home")
-    } else{
-        res.redirect("/")
-    }
+const auth = async (req, res) => {
+    let user
+    
+    await Manager.find({
+            username: req.body.loginusername,
+            password: req.body.loginpassword
+        })
+        .exec()
+        .then((result) => {
+            user = result.length
+            if (user === 1) {
+                res.redirect("/home")
+            } else {
+                res.redirect("/")
+            }
+        })
+        .catch((err) => console.log(err))
 }
+
+module.exports = auth
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const Manager = require("../models/managerModel")
+const auth = require("./auth")
 
 router.get("/", (req, res) => {
     res.render("login")
 })
 
-router.post("/", (req, res) => {
+router.post("/", auth, (req, res) => {
     res.redirect("/home")
 })
 
